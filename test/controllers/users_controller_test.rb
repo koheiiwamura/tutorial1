04dd6a4dc8c8701db394f645b39c9e68e942c09b
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-12-31 01:32:13 +0900

    Finish user signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,18 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should get new" do
-    get signup_path
+    get new_user_path
     assert_response :success
+  end
+  test "valid signup information" do
+    get new_user_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
